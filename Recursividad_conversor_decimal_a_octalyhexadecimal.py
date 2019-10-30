08338920c63aa8e3fc97ef1c6ec64b5000601ea4
Mauricio-Nunez <55606635+Mauricio-Nunez@users.noreply.github.com>
2019-10-30 14:14:28 -0600

    Update Recursividad_conversor_decimal_a_octalyhexadecimal.py
    se creo el método para  convertir un número decimal a hexadecimal

--- Recursividad_conversor_decimal_a_octalyhexadecimal.py
+++ Recursividad_conversor_decimal_a_octalyhexadecimal.py
@@ -8,3 +8,13 @@ def Conversor_Octal(n):    #definimos un método
 print("_____________Octal_______________")
 Conversor_Octal = Conversor_Octal(20)             #al método se le agregará un número al cual se le convertira a base octal
 print(Conversor_Octal)
+
+
+def ConversorHex(n):           #crearemos otro método
+    if (n < 0):             #agregaremos una condicional,si el valor a introoducir es menor a cero
+        print(0)            #imprimira el número cero
+    elif (n<=1):            #si  no se cumple la condición anterior imprimir número en linea
+        print (n, end = '')
+    else:                   #si no se llamará recursivamente al método que creamos
+        ConversorHex(int( n / 16 ))#el cual convertimos en entero para que no nos salgan puntos decimales y el número a introducir un número lo dividimos entre la base 16
+        x =(int(n%16))      #creamos una variable la cual contendra el número de el método le sacaremos su residuo
