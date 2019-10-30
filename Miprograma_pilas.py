3e2656f3284f0e3e086f30aa78b107a9809b2ce6
Mauricio-Nunez <55606635+Mauricio-Nunez@users.noreply.github.com>
2019-10-30 15:21:32 -0600

    Update Miprograma_pilas.py
    se agregaron condicionales para insertar y quitar elementos

--- Miprograma_pilas.py
+++ Miprograma_pilas.py
@@ -9,5 +9,14 @@ class pila:#se crea una clase
         if self.top < self.cantidad :# esta instrucción tiene como regla que si el tope es menor a la cantidad
             self.top += 1 #se le podrá agregar elementos
             self.datos.append(dato)  #despues adiciono
+            
+            if dato < 0:#si el dato es menor a cero se quitará
+               self.datos.pop()
+
+            elif (dato %2 != 0):#si no se realizó la anterior operación
+                self.datos.pop()#se realizará esta operación que quitara el dato no es número par
+
+            elif dato == 0:#si no se realizó la anterior operación
+                self.datos.clear() #si dato es cero se eliminará
 
     
