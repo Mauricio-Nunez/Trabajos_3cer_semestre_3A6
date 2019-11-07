d6c7258193f745d7b62849e02d8ac2d8f75748ad
Mauricio-Nunez <55606635+Mauricio-Nunez@users.noreply.github.com>
2019-11-07 14:19:31 -0600

    Update listas_enlazadas_con_polinomios.py
    se agregaron comentarios al programa

--- listas_enlazadas_con_polinomios.py
+++ listas_enlazadas_con_polinomios.py
@@ -1,21 +1,25 @@
-class Nodo:
-    def __init__(self,dato):
-        self.siguiente = None
-        self.info=dato
-    def verNodo(self):
-        return self.info
-class Lista:
-    def __init__(self):
-        self.primero = None
+class Nodo:#se creara la clase llamada nodo
+    def __init__(self,dato):#se le insertó el método constructor
+        self.siguiente = None# se creo la variable siguiente
+        self.info=dato#la clase info se tomara para que sea igual a dato
+    def verNodo(self):# se crea un método para que retorne el dato
+        return self.info#
 
-    #def vacia(self):
-     #   if self.primero == None:
-      #      return True
-       # else:
-        #    return False
+    
+   
 
- def insertarPrimero(self,introducir_dato):
-        temporal = Nodo(introducir_dato)
+class Lista:#se creó otra clase para listar
+    def __init__(self):#se creó el método constructor
+        self.primero = None #el primer dato sera nulo
+
+    def vacia(self):#este método define si la lista esta vacia  
+        if self.primero == None: #si la  variable primero es igual a vacia se retornará verdadero u falso
+            return True
+        else:
+            return False
+
+ def insertarPrimero(self,introducir_dato): #se creó el método  para insertar el primer dato
+        temporal = Nodo(introducir_dato) #se creó  una variable llamada temporal la cual la cual será igual a la clase nodo y introducir datos
         temporal.siguiente = self.primero
         self.primero = temporal
 
